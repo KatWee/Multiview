2ed630f3581db9b169b92788d88934b86df26244
shiroikatsu <34379387+shiroikatsu@users.noreply.github.com>
2020-04-20 22:08:44 +0700

    fill color

--- test.py
+++ test.py
@@ -31,7 +31,24 @@ edges = (
     )
 
 
+surfaces=(
+    (0, 1, 2, 3),
+    (3, 2, 7, 6),
+    (6, 7, 5, 4),
+    (4, 5, 1, 0),
+    (4, 5, 7, 2),
+    (4, 0, 3, 6)
+)
+
+
 def Cube():
+    glBegin(GL_QUADS)
+    glColor3fv((1, 1, 1))
+    for surface in surfaces:
+        for vertex in surface:
+            glVertex3fv(verticies[vertex])
+    glEnd()
+
     glBegin(GL_LINES)
     for edge in edges:
         for vertex in edge:
@@ -48,13 +65,14 @@ def main():
 
     glTranslatef(0.0,0.0, -5)
 
+    glRotatef(30, 0, -1, 0)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
 
-        glRotatef(1, 3, 1, 1)
         glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
         Cube()
         pygame.display.flip()
